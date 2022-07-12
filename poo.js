873d6c96af47c71e56152338f1ef383e2b3b5138
nvformateurdwdm <nvformateurdwdm@gmail.com>
2022-07-12 10:33:46 -0400

    Classe Oiseau OK.

--- poo.js
+++ poo.js
@@ -1,27 +1,47 @@
-    class Animal{
-        constructor(nom, couleur, sante = 100){
-            this.nom = nom;
-            this.couleur = couleur;
-            this.sante = sante;
-        }
-
-        dormir(){
-            console.log("L'animal " + this.nom + " dort.");
-        }
-
-        manger(){
-            console.log("L'animal " + this.nom + " mange.");
-        }
-
-        attaquer(){
-            console.log("L'animal " + this.nom + " attaque.");
-        }
-
-        seDeplacer(){
-            console.log("L'animal " + this.nom + " se déplace.");
-        }
-
-        communiquer(){
-            console.log("L'animal " + this.nom + " communique.");
-        }
+class Animal {
+    constructor(nom, couleur, sante = 100) {
+        this.nom = nom;
+        this.couleur = couleur;
+        this.sante = sante;
     }
+
+    dormir() {
+        console.log("L'animal " + this.nom + " dort.");
+    }
+
+    manger() {
+        console.log("L'animal " + this.nom + " mange.");
+    }
+
+    attaquer() {
+        console.log("L'animal " + this.nom + " attaque.");
+    }
+
+    seDeplacer() {
+        console.log("L'animal " + this.nom + " se déplace.");
+    }
+
+    communiquer() {
+        console.log("L'animal " + this.nom + " communique.");
+    }
+}
+
+class Oiseau extends Animal {
+    constructor(nom, couleur, sante = 100, longueurDesAiles){
+        super(nom, couleur, sante);
+
+        this.longueurDesAiles = longueurDesAiles;
+    }
+
+    voler(){
+        super.seDeplacer();
+        console.log("L'oiseau " + this.nom + " vole.");
+    }
+
+    chanter(){
+        super.communiquer();
+        console.log("L'oiseau " + this.nom + " chante.");
+    }
+
+
+}
